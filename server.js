d556c91726f8394a3de2764524a5f94227e6a7c0
claytonbjerke <cbjerkeuco@gmail.com>
2016-04-03 13:47:15 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 8000));
 
-app.get('/', function(req, res) {
-  res.send('hi lol!');
-});
+app.use(express.static(__dirname));
+// app.get('/', function(req, res) {
+//   res.static(__dirname);
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Listening on port: ', app.get('port'));
